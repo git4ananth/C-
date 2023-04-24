8d9d5abf8a25624db89ecd5d3de435a850009791
git4ananth <114994781+git4ananth@users.noreply.github.com>
2023-04-24 09:39:51 +0530

    Initial Commit

---
+++ search_in_array.cpp
@@ -0,0 +1,27 @@
+// Can you search for an element in a given array? Try to print that number if present else printf -1.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {23, 65, 34, 98, 77, 3, 22, 1, 54};
+    int search_element;
+    bool flag;
+    cout << "Enter an element to search in Array : ";
+    cin >> search_element;
+
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    {
+        if (arr[i] == search_element)
+        {
+            flag = true;
+            cout << "Element found at index " << i << "." << endl;
+            break;
+        }
+    }
+    if (flag == false)
+    {
+        cout << "Element not in Array";
+    }
+}
