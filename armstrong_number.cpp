7c55aae0df5fcc2a833176e4d0f1e996c8a0a824
git4ananth <114994781+git4ananth@users.noreply.github.com>
2023-04-24 09:21:41 +0530

    Initial Commit

---
+++ armstrong_number.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter num to check if its an armstrong num : ";
+    cin >> num;
+    int num_dup, temp, sum = 0, num_digits = 0;
+    num_dup = num;
+    if (num > 0)
+    {
+        while (num_dup)
+        {
+            num_digits++;
+            num_dup /= 10;
+        }
+
+        num_dup = num;
+
+        while (num_dup)
+        {
+            temp = num_dup % 10;
+            sum = sum + pow(temp, num_digits);
+            // cout << "sum is : " << sum;
+            num_dup /= 10;
+        }
+
+        if (num == sum)
+        {
+            cout << "Armstrong Number" << endl;
+        }
+        else
+        {
+            cout << "Not an Armstrong Number" << endl;
+        }
+    }
+    else{
+        cout << "Enter a positive number" << endl;
+    }
+}
