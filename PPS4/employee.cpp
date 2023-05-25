dba119614680cd93af277590b0d95b8a82f92c60
git4ananth <114994781+git4ananth@users.noreply.github.com>
2023-05-25 10:42:45 +0530

    Employee Details using Hybrid Inheritance

--- PPS4/employee.cpp
+++ PPS4/employee.cpp
@@ -9,18 +9,17 @@ class Employee
     string designation;
 
 public:
-    Employee() {}
     Employee(int emp_no, string emp_name, string designation)
     {
         this->emp_no = emp_no;
         this->emp_name = emp_name;
         this->designation = designation;
     }
-    void printEmp()
+    virtual void print()
     {
-        cout << "empno" << emp_no << endl;
-        cout << "emp_name" << emp_name << endl;
-        cout << "designation" << designation << endl;
+        cout << "Emp number:" << emp_no << endl;
+        cout << "Emp Name:" << emp_name << endl;
+        cout << "Designation:" << designation << endl;
     }
 };
 
@@ -29,35 +28,42 @@ class Salary : private Employee
     float hra, da, pf;
 
 public:
-    Salary(int hra, int da, int pf)
+    Salary(int emp_no, string emp_name, string designation,
+           int hra, int da, int pf)
+        : Employee(emp_no, emp_name, designation)
     {
         this->hra = hra;
         this->da = da;
         this->pf = pf;
     }
-    void printSalary()
+    virtual void print()
     {
+        Employee ::print();
         cout << "hra" << hra << endl;
         cout << "da" << da << endl;
         cout << "pf" << pf << endl;
     }
 };
 
-class Bank_details
+class Bank_details : private Salary
 {
     string bank_name;
     int acc_no;
 
 public:
-    Bank_details(string bank_name, int acc_no)
+    Bank_details(int emp_no, string emp_name, string designation, int hra, int da, int pf,
+    string bank_name, int acc_no)
+    : Salary(emp_no, emp_name, designation, hra, da, pf)
     {
         this->acc_no = acc_no;
         this->bank_name = bank_name;
     }
-    void printBank()
+    void print()
     {
-        cout << "acc_no" << acc_no << endl;
-        cout << "bank_name" << bank_name << endl;
+        //anti-pattern -> defeats purpose of OOP
+        Salary ::print();
+        cout << "Bank Name: " << bank_name << endl;
+        cout << "Account Number: " << acc_no << endl;
     }
 };
 
@@ -85,10 +91,14 @@ int main()
     cout << "Enter account number ";
     cin >> acc_no;
 
-    Employee e1(emp_no, emp_name, designation);
-    Salary s1(hra, da, pf);
-    Bank_details b1(bank_name, acc_no);
-    e1.printEmp();
-    s1.printSalary();
-    b1.printBank();
+    // Employee e1(emp_no, emp_name, designation);
+    // Salary s1(hra, da, pf);
+    // Bank_details b1(bank_name, acc_no);
+    // e1.printEmp();
+    // s1.printSalary();
+    // b1.printBank();
+
+    Bank_details b1(emp_no, emp_name, designation, hra, da, pf, bank_name, acc_no);
+    b1.print();
+
 }
